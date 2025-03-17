60e73d12ea75fb0597165f24ada407602035bb60
diolemos <pedro.devsc@gamil.com>
2025-03-16 22:57:54 -0300

    update palyndrome.php

---
+++ palyndrome.php
@@ -0,0 +1,13 @@
+<?php 
+    declare(strict_types = 1);
+
+    function longestPalindromicSubstring(string $s){
+        if(!$s or strlen($s) < 1 ) return "";
+
+        $longest = "";
+        
+        for($i=0;$i<strlen($s);$i++){
+            $oddPalindrome = "" ;
+            $evenPalindrome = "";
+        }
+    }
