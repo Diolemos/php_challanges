eff46b97c1b268c9edc9d0b011af4afa5b95d263
diolemos <pedro.devsc@gamil.com>
2025-03-16 23:05:45 -0300

    update palyndrome.php create expandAroundCenter function

--- palyndrome.php
+++ palyndrome.php
@@ -11,3 +11,12 @@
             $evenPalindrome = "";
         }
     }
+
+
+     function expandAroundCenter($s,$left,$right){ 
+        while ($left >= 0 && $right < strlen($s) && $s[$left] === $s[$right]) {
+            $left--;
+            $right++;
+        }
+        return  substr($s,$left+1,($right - $left - 1)) ;
+    }
